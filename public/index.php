0cb6b301fcbe4161742d3c517d5873368c396108
LazarevDev <lazarev.w3b@yandex.ru>
2024-04-21 16:44:59 +0300

    update docker

--- public/index.php
+++ public/index.php
@@ -1,17 +1,3 @@
-<?php
-$servername = "mysql";
-$username = "myuser";
-$password = "mypassword";
-$dbname = "mydatabase";
-
-// Создаем соединение
-$conn = new mysqli($servername, $username, $password, $dbname);
-
-// Проверяем соединение
-if ($conn->connect_error) {
-  die("Connection failed: " . $conn->connect_error);
-}
-
-echo "Соединение успешно установлено!";
+<?php 
+phpinfo();
 ?>
-
